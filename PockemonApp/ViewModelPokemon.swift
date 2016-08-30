9ee19aaa0f2bb6e9c5811a68a9d45c2e83e4d96c
Anna Gorobchenko <annagorobchenko@mac-mini-anna.local>
2016-08-30 11:23:35 +0300

    cleaning

--- PockemonApp/ViewModelPokemon.swift
+++ PockemonApp/ViewModelPokemon.swift
@@ -13,106 +13,35 @@ import RxCocoa
 
 
 class ViewModelPokemon {
-
-   // private let pokemon: Pokemon
     
     var modelName: Variable <String> = Variable( "" )
     var modelWeight: Variable <String> = Variable( "" )
     
     private let apiService = ApiService()
     private let apiServiceGetPokemons = ApiServiceGetPokemons()
-   // private let bag = DisposeBag()
-    
+       
     var pokemons: Observable<[Pokemon]?>?
-     /*
-    init (pokemon : Pokemon) {
-    
-        self.pokemon = pokemon
-    
-        modelName = BehaviorSubject<String>(value: pokemon.name!)
-        modelName.subscribeNext { (name) in
-        pokemon.name = name
-        }.addDisposableTo(bag)
-        
-        
-        
-        modelWeight = BehaviorSubject<String>(value: pokemon.weight!)
-        modelWeight.subscribeNext { (weight) in
-            pokemon.weight = weight
-            }.addDisposableTo(bag)
-    }
-    
- 
-    init() {
-        // ...
-        
-        self.pokemons = self.apiService.getPokemonsUrlsNames(20)
-            // Get the countries corresponding to the alpha codes
-            // specified in the `borders` property
-            .flatMap { url in
-                self.apiServiceGetPokemons.getPokemons(url!, count: 20)
-            }
-            // Catch any error and print it in the console
-            .catchError { error in
-                print("Error: \(error)")
-                return Observable.just([])
-            }
-            // Transform the resulting countries into [Border]
-           // .map { countries in
-                //countries.map { (name: $0.name, nativeName: $0.nativeName) }
-           // }
-            // Make sure events are delivered in the main thread
-            .observeOn(MainScheduler.instance)
-            // Make sure multiple subscriptions share the side effects
-            .shareReplay(1)
-    }
-
-    */
     
     var observableApiGetPokemons : Observable <[Pokemon]?>? {
         
         didSet {
- 
+            
             if let value = self.apiService.getPokemonsUrlsNames(20) {
                 self.pokemons = value
-               
-                .flatMap { url in
-                    self.apiServiceGetPokemons.getPokemons(url!, count: 20)
-                }
-                // Catch any error and print it in the console
-                .catchError { error in
-                    print("Error: \(error)")
-                    return Observable.just([])
-                }
                     
-                // Transform the resulting countries into [Border]
-                // .map { pokemon in
-                //    pokemons.map { (modelName.value: $0., modelWeight: $0) }
-               //  }
-                // Make sure events are delivered in the main thread
-                .observeOn(MainScheduler.instance)
-               /* .subscribe(
-                    onNext: { pokemons in
-                       
-                            modelName.value = pokemons.
-                            
-                        
-                    },
-                    
-                    onError: { pokemon in
-                        
-                        
+                    .flatMap { url in
+                        self.apiServiceGetPokemons.getPokemons(url!, count: 20)
                     }
                     
-                )
-                 */
+                    .catchError { error in
+                        print("Error: \(error)")
+                        return Observable.just([])
+                    }
+                    
+                    .observeOn(MainScheduler.instance)
+            }
             
         }
-                // Make sure multiple subscriptions share the side effects
-        
-
-
-    }
     }
 }
 
